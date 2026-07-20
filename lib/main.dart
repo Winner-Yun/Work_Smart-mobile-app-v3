020fbbf0608a4f19c62436700e38731a5731d207
Your Name <your-email@example.com>
2026-07-20 10:01:22 +0700

    add new

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:flutter_worksmart_app/shared/widget/user/restartwidget.dart';
 
 void main() async {
   // Setup app and launch.
-  debugPrint('🚀 Starting app...');
   final initialRoute = await AppBootstrap.init();
   runApp(RestartWidget(child: MainApp(initialRoute: initialRoute)));
 }
